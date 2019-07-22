c2a32ef62746c96be7f92f4d4e68882fca90933a
Dimitar Bakardzhiev <dimiter.bakardzhiev@code-runnders.com>
2019-07-22 18:52:03 +0300

    setup database

---
+++ Vimata.Data/VimataDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Vimata.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    public class VimataDbContext : DbContext
+    {
+        public VimataDbContext(DbContextOptions<VimataDbContext> options) : base(options)
+        {
+        }
+    }
+}
